59d42b9a2dfe9b70ab3988b070a000c3a42871f5
Hank-Tsou <hank630280888@gmail.com>
2019-05-04 23:00:49 -0700

    video get info

---
+++ tutorials/Basic_Operation/Video_get_info/README.md
@@ -0,0 +1,6 @@
+# Basic_Feature
+
+## Descrption:
+- know about computer vision perspective on digital image and video
+- know how to do image and video input and output operation
+- know how to get image and video information by using OpenCV function
---
+++ tutorials/Basic_Operation/Video_get_info/video_get_info.py
@@ -0,0 +1,48 @@
+#------------------------------------#
+# Author: Yueh-Lin Tsou              #
+# Update: 5/4/2019                   #
+# E-mail: hank630280888@gmail.com    #
+#------------------------------------#
+
+"""---------------------------------
+1. Get video information
+---------------------------------"""
+
+# Import OpenCV Library
+import cv2
+
+# ------------------------- Get Video source --------------------------#
+
+# get video source from video file
+cap = cv2.VideoCapture("cat.mp4")
+
+# get video source from webcam
+# cap = cv2.VideoCapture(0)
+
+# ---------------------------------------------------------------------#
+
+print("\ncheck video capture: ", cap.isOpened())
+
+# ----------------------- Get Video information -----------------------#
+
+if(cap.isOpened()):
+    print("\n-------- video information --------")
+    print("frame width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    print("frame width: ", cap.get(3))
+
+    print("frame height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print("frame height: ", cap.get(4))
+
+    print("Total number of frames: ", cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print("Total number of frames: ", cap.get(7))
+
+    print("FPS: ", cap.get(cv2.CAP_PROP_FPS))
+    print("FPS: ", cap.get(5))
+    print("-----------------------------------")
+
+
+
+# Reference:
+# Website: Opencv-Python Tutorials
+# Link: https://opencv-python-tutroals.readthedocs.io/
+# en/latest/py_tutorials/py_tutorials.html
