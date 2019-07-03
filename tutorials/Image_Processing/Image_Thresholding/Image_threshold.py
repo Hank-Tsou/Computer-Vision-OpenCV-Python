93d9cf62f740fece7e04d5624c9d43d336ea0094
Hank-Tsou <hank630280888@gmail.com>
2019-07-03 21:39:19 +0800

    thresh_unfinish

---
+++ tutorials/Image_Processing/Image_Thresholding/Image_threshold.py
@@ -0,0 +1,106 @@
+
+#------------------------------------#
+# Author: Yueh-Lin Tsou              #
+# Update: 7/2/2019                   #
+# E-mail: hank630280888@gmail.com    #
+#------------------------------------#
+
+"""----------------------------------------
+- Simple thresholding   (sim_thresh())
+- Adaptive thresholding (ada_thresh())
+- Otsu’s thresholding   (otsu_thresh())
+----------------------------------------"""
+
+# Import OpenCV Library, numpy and command line interface
+import cv2
+import numpy as np
+import argparse
+from matplotlib import pyplot as plt
+
+# ----- Function to convert RGB imge to grayscale image ----- #
+def sim_thresh(img):
+
+    ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+    ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+    ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+    ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+
+    titles = ['Origin','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+    images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+    for i in range(6):
+        plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+
+    plt.show()
+
+def ada_thresh(img):
+
+    img = cv2.medianBlur(img,5)
+
+    ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,
+                cv2.THRESH_BINARY,11,2)
+    th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                cv2.THRESH_BINARY,11,2)
+
+    titles = ['Original Image', 'Global Thresholding (v = 127)',
+                'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+    images = [img, th1, th2, th3]
+
+    for i in range(4):
+        plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
+
+def otsu_thresh(img):
+
+    # global thresholding
+    ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    # Otsu's thresholding
+    ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # Otsu's thresholding after Gaussian filtering
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # plot all the images and their histograms
+    images = [img, 0, th1,
+              img, 0, th2,
+              blur, 0, th3]
+    titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+              'Original Noisy Image','Histogram',"Otsu's Thresholding",
+              'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+    for i in range(3):
+        plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+        plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+        plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+        plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+# -------------------------- main -------------------------- #
+if __name__ == '__main__':
+    # read one input from terminal
+    # (1) command line for sim and ada >> python Image_threshold.py -i thresh.jpg
+    # (2) command line for otsu >> python Image_threshold.py -i noise.jpg
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path to the input image")
+    args = vars(ap.parse_args())
+
+    # Read image
+    image = cv2.imread(args["image"])
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Functions, the input image should be grayscale image
+    # sim_thresh(image)
+    # ada_thresh(image)
+    otsu_thresh(image)
+
+
+
+# Reference:
+# Website: OpenCV-Python Document
+# Link: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html
