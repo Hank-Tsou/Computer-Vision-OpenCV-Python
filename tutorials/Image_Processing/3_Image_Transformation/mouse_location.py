7387dcfc4842334d08e1ee948d3c50e14b5f2330
Hank-Tsou <hank630280888@gmail.com>
2019-07-12 16:14:48 +0800

    mouse loc

--- tutorials/Image_Processing/3_Image_Transformation/mouse_location.py
+++ tutorials/Image_Processing/3_Image_Transformation/mouse_location.py
@@ -1,19 +1,57 @@
+
+#------------------------------------#
+# Author: Yueh-Lin Tsou              #
+# Update: 7/10/2019                  #
+# E-mail: hank630280888@gmail.com    #
+#------------------------------------#
+
+"""-----------------------------------------------------------
+Get mouse location on an image
+    - Can use this program to help do image transformation
+-----------------------------------------------------------"""
+
+# Import OpenCV Library, command line interface
 import cv2
+import argparse
 
-def draw_circle(event,x,y,flags,param):
-    global mouseX,mouseY
+# ------------------- Function to get mouse location on an image ------------------- #
+def get_location(event, x, y, flags, param):
+    global loc_x, loc_y # location parameter
+    # mouse left click --> draw dot and get location
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img,(x,y),100,(255,0,0),-1)
-        mouseX,mouseY = x,y
+        cv2.circle(img,(x,y),10,(255,0,0),-1)
+        loc_x = x
+        loc_y = y
 
-img = cv2.imread("perspect_img.png")
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_circle)
+# -------------------------- main -------------------------- #
+if __name__ == '__main__':
+    # read one input from terminal
+    # Step: (1) left click on the image location (2) press 'a' to show location on Terminal
+    # command line >> python mouse_location.py -i perspect_img.png
 
-while(1):
-    cv2.imshow('image',img)
-    k = cv2.waitKey(20) & 0xFF
-    if k == 27:
-        break
-    elif k == ord('a'):
-        print (mouseX,mouseY)
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path to the input image")
+    args = vars(ap.parse_args())
+
+    # Read image
+    img = cv2.imread(args["image"])
+    cv2.namedWindow('image')
+
+    # function to get the location
+    cv2.setMouseCallback('image',get_location)
+
+    while(1):
+        cv2.imshow('image',img)
+        k = cv2.waitKey(20)
+
+        # Esc key to end program
+        if k == 27:
+            break
+
+        # press 'a' to print the location
+        elif k == ord('a'):
+            print (loc_x,loc_y)
+
+# Reference:
+# Website: OpenCV-Python Document
+# Link: https://stackoverflow.com/questions/14494101/using-other-keys-for-the-waitkey-function-of-opencv
