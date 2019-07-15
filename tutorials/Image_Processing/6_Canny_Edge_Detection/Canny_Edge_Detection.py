d1c969d2747b54350177ed8906b8633e26e40c06
Hank-Tsou <hank630280888@gmail.com>
2019-07-15 13:46:40 +0800

    image

--- tutorials/Image_Processing/6_Canny_Edge_Detection/Canny_Edge_Detection.py
+++ tutorials/Image_Processing/6_Canny_Edge_Detection/Canny_Edge_Detection.py
@@ -15,24 +15,29 @@ import numpy as np
 import argparse
 from matplotlib import pyplot as plt
 
-# ------------------- Function to do Image Gradient ------------------- #
+# ------------------- Function to do Canny Edge Detection ------------------- #
 def Canny_Edge_Detection(img):
+    # Canny edge detector
+    edges = cv2.Canny(img,100,200)
 
-    cv2.imshow("image", img)
-    cv2.waitKey(1000)
-    cv2.destroyAllWindow()
+    # show result
+    plt.subplot(121),plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
+    plt.show()
 
 # -------------------------- main -------------------------- #
 if __name__ == '__main__':
     # read one input from terminal
-    # (1) command line >> python Image_Gradient.py -i chess_board.png
+    # (1) command line >> python Canny_Edge_Detection.py  -i Lenna.jpg
 
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True, help="Path to the input image")
     args = vars(ap.parse_args())
 
-    # Read image and convert to grayscale image
+    # Read image
     image = cv2.imread(args["image"])
 
     ## Functions
